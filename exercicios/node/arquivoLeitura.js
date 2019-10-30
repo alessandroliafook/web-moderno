3e828d80f4379d9a57066980e1011099e2f31928
alessandro.l.santos <kRf*m3gI>
2019-10-30 20:18:10 -0300

    Finalizando aula 32 da seção 8 NODE

--- exercicios/node/arquivoLeitura.js
+++ exercicios/node/arquivoLeitura.js
@@ -1,1 +1,27 @@
 const fs = require('fs')
+
+// __dirname está presente em todos os arquivos do node e
+// contem o diretório atual relativo ao arquivo em que é chamado
+const caminho = __dirname + '/arquivo.json'
+
+//sincrono... trava o event loop até o final de sua execução
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+//assincrono
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    if (err === null) {
+        const config = JSON.parse(conteudo)
+        console.log(`${config.db.host}:${config.db.port}`)
+    }
+})
+
+const config = require('./arquivo.json')
+console.log(config.db)
+
+fs.readdir(__dirname, (err, arquivos) => {
+    if(err === null) {
+        console.log('Conteúdo da pasta...')
+        console.log(arquivos)
+    }
+})
